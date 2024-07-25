cdc83b799b3995ee1370488b27b1ebb6d9fd0205
jjohmin <jhmin7624@gmail.com>
2024-07-26 01:59:37 +0900

    ⚡️ :: Page_Change

--- plan_b/lib/main.dart
+++ plan_b/lib/main.dart
@@ -7,43 +7,186 @@ void main() {
 }
 
 class App extends StatefulWidget{
+  App({super.key});
+
   @override
   State<App> createState() => _AppState();
 }
 
-class _AppState extends State<App> {
-  var _index = 0;
+class OSJIconButton extends StatelessWidget {
+  const OSJIconButton(
+      {super.key,
+        required this.width,
+        required this.height,
+        required this.iconSize,
+        required this.color,
+        required this.iconColor,
+        required this.iconData,
+        this.function});
 
-  List _pages = [
-    Home(),
-    Map(),
-  ];
+  final double width, height, iconSize;
+  final Color color, iconColor;
+  final IconData iconData;
+  final Function? function;
 
   @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: _pages[_index],
-          bottomNavigationBar: BottomNavigationBar(
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            iconSize: 30,
-            unselectedItemColor: Color(0xFFEBEBEB),
-            selectedItemColor: Color(0xFF27AB00),
-            backgroundColor: Colors.white,
-            elevation: 0.0,
-            currentIndex: _index,
-            onTap: (value) {
-              setState(() {
-                _index = value;
-              });
-            },
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-              BottomNavigationBarItem(icon: Image.asset('assets/images/metaverse2.png'), label: 'map',activeIcon: Image.asset('assets/images/metaverse3.png')),
-            ],),
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function != null ? () => function : null,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16.0),
         ),
-      );
+        child: Center(
+          child: Icon(iconData, size: iconSize, color: iconColor),
+        ),
+      ),
+    );
+  }
+}
+
+class OSJImageButton extends StatelessWidget {
+  const OSJImageButton({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.color,
+    required this.imagePath,
+    this.function,
+    this.imageWidth,
+    this.imageHeight,
+  });
+
+  final double width, height;
+  final double? imageWidth, imageHeight;
+  final Color color;
+  final String imagePath;
+  final Function? function;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: function != null ? () => function : null,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Center(
+          child: Image.asset(
+            imagePath,
+            width: imageWidth ?? 24.0,
+            height: imageHeight ?? 24.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _AppState extends State<App> with SingleTickerProviderStateMixin{
+  late TabController controller;
+  int selectedIndex = 1;
+  bool isChange = false;
+
+  void initState(){
+    super.initState();
+    controller = TabController(length: 2, vsync: this)
+      ..index = 1
+      ..animation?.addListener(() {
+        if (controller.offset >= 0.5 &&
+            controller.offset < 1.0 &&
+            isChange == false) {
+          setState(() {
+            isChange = true;
+            selectedIndex = 1;
+          });
+        }
+        if (controller.offset < 0.5 &&
+            controller.offset > 0.0 &&
+            isChange == true) {
+          setState(() {
+            isChange = false;
+            selectedIndex = 0;
+          });
+        }
+        if (controller.offset >= -0.5 &&
+            controller.offset < 0.0 &&
+            isChange == false) {
+          setState(() {
+            isChange = true;
+            selectedIndex = 1;
+          });
+        }
+        if (controller.offset < -0.5 &&
+            controller.offset > -1.0 &&
+            isChange == true) {
+          setState(() {
+            isChange = false;
+            selectedIndex = 0;
+          });
+        }
+        if (controller.offset == 1.0 || controller.offset == 0.0) {
+          setState(() {
+            isChange = false;
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : Scaffold(
+        backgroundColor: Colors.white,
+        body: TabBarView(
+          controller: controller,
+          children: [
+            Home(),
+            Map(),
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          padding: EdgeInsets.only(top: 10.0, bottom: 30.0),
+          dividerColor: Colors.transparent,
+          controller: controller,
+          indicatorColor: Colors.transparent,
+          tabs: [
+            OSJIconButton(
+                width: 185.0,
+                height: 48.0,
+                iconSize: 24.0,
+                color: selectedIndex == 0
+                    ? Color(0xFFEBEBEB)
+                    : Colors.white,
+                iconColor: selectedIndex == 0
+                    ? Color(0xFF27AB00)
+                    : Color(0xFFEBEBEB),
+                iconData: Icons.home),
+            OSJImageButton(
+              width: 185.0,
+              height: 48.0,
+              color:
+              selectedIndex == 1 ? Color(0xFFEBEBEB) : Colors.white,
+              imagePath: selectedIndex == 1
+                  ? "assets/images/metaverse3.png"
+                  : "assets/images/metaverse2.png",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- plan_b/lib/src/pages/naon.dart
+++ plan_b/lib/src/pages/naon.dart
@@ -7,6 +7,7 @@ class naon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFFFFFFFF),
         body: SingleChildScrollView(
